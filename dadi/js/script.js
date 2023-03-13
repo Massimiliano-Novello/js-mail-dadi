6b26d15eaf842ad5bd9de524277571f2eb322658
Massimiliano-Novello <massinov5@gmail.com>
2023-03-13 19:32:09 +0100

    stile nell'esercizio dei dadi

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,26 +1,35 @@
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
 
+const submitBtn = document.querySelector(".genera");
+submitBtn.addEventListener("click", function() {
+
 
 //Generare numeri random
 const number = Math.floor(Math.random() * 6) + 1;
 console.log(number);
 
+document.getElementById("computer-generate").innerHTML = `Il numero del computer è ${number}`
+
 const userNumber = Math.floor(Math.random() * 6) + 1;
 console.log(userNumber);
 
+document.getElementById("user-generate").innerHTML = `Il numero dell'utente è ${userNumber}`
 
-//Logcica
+
+
+
+//Logica
 let message;
 if (number > userNumber) {
         message = "vince il computer";
         document.querySelector(".result").classList.add("number")
 } else {
         message = "vince l'utente";
-        document.querySelector(".result").classList.add("user-number")
+        document.querySelector(".result").classList.add("user-number");
 }
 
 //Stampare il risultato
 
 document.querySelector(".result").innerHTML = message;
-
+})
